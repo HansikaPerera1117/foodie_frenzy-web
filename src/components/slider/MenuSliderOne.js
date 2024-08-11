57f6efadb6fd679be0f28e380998563dc5eccbae
HansikaPerera1117 <hansikaperera59@gmail.com>
2024-08-11 10:59:11 +0530

    update menu slider code

--- src/components/slider/MenuSliderOne.js
+++ src/components/slider/MenuSliderOne.js
@@ -1,89 +1,125 @@
 import Link from "next/link";
 import Slider from "react-slick";
 import { menuSliderOne } from "../../sliderProps";
+import { useEffect, useState } from "react";
 const MenuSliderOne = () => {
+  const [menuSlider, setMenuSlider] = useState([]);
+
+  useEffect(() => {
+    setMenuSlider([
+      {
+        id: 1,
+        slide: (
+          <div className="menu-grid-item-one text-center mb-60">
+            <div className="thumb">
+              <img src="assets/images/menu/menu-1.png" alt="" />
+            </div>
+            <div className="text">
+              <h3 className="title">
+                <Link legacyBehavior href="/product-details">
+                  Steamed Crabs Dish
+                </Link>
+              </h3>
+              <p>Roasted langoustine, consommé</p>
+              <span className="price">
+                <span className="currency">$</span>253.59
+              </span>
+            </div>
+          </div>
+        ),
+      },
+      {
+        id: 2,
+        slide: (
+          <div className="menu-grid-item-one text-center mb-60">
+            <div className="thumb">
+              <img src="assets/images/menu/menu-2.png" alt="" />
+            </div>
+            <div className="text">
+              <h3 className="title">
+                <Link legacyBehavior href="/product-details">
+                  Steamed Crabs Dish
+                </Link>
+              </h3>
+              <p>Roasted langoustine, consommé</p>
+              <span className="price">
+                <span className="currency">$</span>253.59
+              </span>
+            </div>
+          </div>
+        ),
+      },
+      {
+        id: 3,
+        slide: (
+          <div className="menu-grid-item-one text-center mb-60">
+            <div className="thumb">
+              <img src="assets/images/menu/menu-3.png" alt="" />
+            </div>
+            <div className="text">
+              <h3 className="title">
+                <Link legacyBehavior href="/product-details">
+                  Steamed Crabs Dish
+                </Link>
+              </h3>
+              <p>Roasted langoustine, consommé</p>
+              <span className="price">
+                <span className="currency">$</span>253.59
+              </span>
+            </div>
+          </div>
+        ),
+      },
+      {
+        id: 4,
+        slide: (
+          <div className="menu-grid-item-one text-center mb-60">
+            <div className="thumb">
+              <img src="assets/images/menu/menu-4.png" alt="" />
+            </div>
+            <div className="text">
+              <h3 className="title">
+                <Link legacyBehavior href="/product-details">
+                  Steamed Crabs Dish
+                </Link>
+              </h3>
+              <p>Roasted langoustine, consommé</p>
+              <span className="price">
+                <span className="currency">$</span>253.59
+              </span>
+            </div>
+          </div>
+        ),
+      },
+      {
+        id: 5,
+        slide: (
+          <div className="menu-grid-item-one text-center mb-60">
+            <div className="thumb">
+              <img src="assets/images/menu/menu-5.png" alt="" />
+            </div>
+            <div className="text">
+              <h3 className="title">
+                <Link legacyBehavior href="/product-details">
+                  Steamed Crabs Dish
+                </Link>
+              </h3>
+              <p>Roasted langoustine, consommé</p>
+              <span className="price">
+                <span className="currency">$</span>253.59
+              </span>
+            </div>
+          </div>
+        ),
+      },
+    ]);
+  }, []);
+
   return (
     <Slider {...menuSliderOne} className="menu-slider-one wow fadeInUp">
-      <div className="menu-grid-item-one text-center mb-60">
-        <div className="thumb">
-          <img src="assets/images/menu/menu-1.png" alt="" />
-        </div>
-        <div className="text">
-          <h3 className="title">
-            <Link legacyBehavior href="/product-details">
-              Steamed Crabs Dish
-            </Link>
-          </h3>
-          <p>Roasted langoustine, consommé</p>
-          <span className="price">
-            <span className="currency">$</span>253.59
-          </span>
-        </div>
-      </div>
-      <div className="menu-grid-item-one text-center mb-60">
-        <div className="thumb">
-          <img src="assets/images/menu/menu-2.png" alt="" />
-        </div>
-        <div className="text">
-          <h3 className="title">
-            <Link legacyBehavior href="/product-details">
-              Steamed Crabs Dish
-            </Link>
-          </h3>
-          <p>Roasted langoustine, consommé</p>
-          <span className="price">
-            <span className="currency">$</span>253.59
-          </span>
-        </div>
-      </div>
-      <div className="menu-grid-item-one text-center mb-60">
-        <div className="thumb">
-          <img src="assets/images/menu/menu-3.png" alt="" />
-        </div>
-        <div className="text">
-          <h3 className="title">
-            <Link legacyBehavior href="/product-details">
-              Steamed Crabs Dish
-            </Link>
-          </h3>
-          <p>Roasted langoustine, consommé</p>
-          <span className="price">
-            <span className="currency">$</span>253.59
-          </span>
-        </div>
-      </div>
-      <div className="menu-grid-item-one text-center mb-60">
-        <div className="thumb">
-          <img src="assets/images/menu/menu-4.png" alt="" />
-        </div>
-        <div className="text">
-          <h3 className="title">
-            <Link legacyBehavior href="/product-details">
-              Steamed Crabs Dish
-            </Link>
-          </h3>
-          <p>Roasted langoustine, consommé</p>
-          <span className="price">
-            <span className="currency">$</span>253.59
-          </span>
-        </div>
-      </div>
-      <div className="menu-grid-item-one text-center mb-60">
-        <div className="thumb">
-          <img src="assets/images/menu/menu-5.png" alt="" />
-        </div>
-        <div className="text">
-          <h3 className="title">
-            <Link legacyBehavior href="/product-details">
-              Steamed Crabs Dish
-            </Link>
-          </h3>
-          <p>Roasted langoustine, consommé</p>
-          <span className="price">
-            <span className="currency">$</span>253.59
-          </span>
-        </div>
-      </div>
+      {menuSlider.map((menu) => {
+        return menu?.slide;
+      })}
     </Slider>
   );
 };
